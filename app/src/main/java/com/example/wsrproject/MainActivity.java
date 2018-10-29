920e530dd043411d583624d80b94fc07b79a78a0
Pavlov Vladimir <vlt18@mail.ru>
2018-10-29 19:57:44 +0700

    1
    2

--- app/src/main/java/com/example/wsrproject/MainActivity.java
+++ app/src/main/java/com/example/wsrproject/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.wsrproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("asasasdasd", "asdserre");
         setContentView(R.layout.activity_main);
+        Log.e("asasasdasd", "asdserre");
+
+
     }
 }
